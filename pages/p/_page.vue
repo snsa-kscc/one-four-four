eef0384dcee7133facf6a4f0edee4a81a856c387
Sinisa Koscec <snsa_kscc@asdf.local>
2021-04-06 02:35:43 +0200

    jos debranding

---
+++ pages/p/_page.vue
@@ -0,0 +1,73 @@
+<template>
+  <grid :posts="posts[$route.params.page - 1]"></grid>
+</template>
+
+<script>
+import _chunk from "lodash/chunk";
+
+export default {
+  async asyncData({ $content, params, error, store }) {
+    const blogPosts = await $content("blog")
+      .sortBy("createdAt", "desc")
+      .only(["title", "path"])
+      .fetch()
+      .catch((err) => {
+        error({ statusCode: 404, message: "Page not found" });
+      });
+    const chunk = _chunk(blogPosts, 12);
+    store.commit("SET_PAGINATION", {
+      active: true,
+      page: params.page,
+      itemsOnPage: chunk[params.page - 1].length,
+      totalItems: blogPosts.length,
+      totalPages: chunk.length,
+    });
+    return {
+      posts: chunk,
+    };
+  },
+  watchQuery: ["page"],
+
+  transition(to, from) {
+    if (!from) return "fade";
+    return +to.query.page > +from.query.page ? "slide-right" : "slide-left";
+  },
+  name: "Index",
+  data() {
+    return {};
+  },
+  methods: {
+    getMore() {},
+  },
+};
+</script>
+
+<style>
+.browse a {
+  width: 100%;
+}
+.search:focus {
+  outline: none;
+}
+.footer__heading {
+  text-transform: uppercase;
+}
+nav .r {
+  grid-gap: 0;
+}
+.r.full-height {
+  grid-gap: 0;
+}
+@media only screen and (max-width: 40rem) {
+  .xs-collapse {
+    visibility: hidden;
+    visibility: collapse;
+    border: 0 !important;
+    border-color: none !important;
+    padding: 0 !important;
+  }
+  .xs-visible {
+    visibility: visible;
+  }
+}
+</style>
