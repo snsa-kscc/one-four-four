c2f209812651358768778603a547619ff23384db
Sinisa Koscec <snsa_kscc@asdf.local>
2021-04-26 02:17:15 +0200

    portfolio gallery

---
+++ components/Gallery.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div
+      class="feat-wrapper"
+      v-for="(galleryImage, i) in galleryImages"
+      :key="`${i}-featimage`"
+    >
+      <transition appear name="fade">
+        <img
+          draggable="false"
+          class="featured-image"
+          loading="lazy"
+          :src="galleryImage"
+          :alt="`${title} 0${i + 1}`"
+        />
+      </transition>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["title", "galleryImages"],
+};
+</script>
--- pages/portfolio/_slug.vue
+++ pages/portfolio/_slug.vue
@@ -11,6 +11,11 @@
             :title="post.title"
             :thumbnail="post.thumbnail"
           />
+          <lazy-gallery
+            v-if="post.galleryImages"
+            :title="post.title"
+            :galleryImages="post.galleryImages"
+          />
 
           <h1 class="xs-py3 main-title">{{ post.title }}</h1>
           <div class="xs-mt-5 bold">
