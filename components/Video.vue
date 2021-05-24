05226be5b0d1978495702eb01235f617be0f22bc
Sinisa Koscec <snsa_kscc@asdf.local>
2021-05-24 16:50:55 +0200

    video dev

---
+++ components/Video.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="featured mb-5 md:mb-12" :key="`${title}-video`">
+    <transition appear name="fade">
+      <video class="featured-image" :src="video" controls />
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  props: ["title", "video"],
+};
+</script>
